6d26df77a5679a7d4cd2c92f820ed90a02c4d771
Himanshu Kumar Gupta <himanshukumargupta135@gmail.com>
2025-07-07 05:03:05 +0530

    Interface with Main method

---
+++ Interface/Interface3.java
@@ -0,0 +1,8 @@
+interface Interface3 {
+
+    //as main method is public static
+    //however this is not common practice
+    static void main(String[] args) {
+        System.out.println("Main method inside interface!");
+    }
+}
