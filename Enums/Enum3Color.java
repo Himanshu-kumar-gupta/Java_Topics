164ca7353f59cb7422e4bf6b4add466bf68bd476
Himanshu Kumar Gupta <himanshukumargupta135@gmail.com>
2025-07-07 02:47:04 +0530

    Enum with Main method

---
+++ Enums/Enum3Color.java
@@ -0,0 +1,11 @@
+public enum Enum3Color {
+    RED,
+    GREEN,
+    BLUE;
+
+    // Main method in enum
+    public static void main(String[] args) {
+        Enum3Color c1 = Enum3Color.RED;
+        System.out.println(c1);
+    }
+}
