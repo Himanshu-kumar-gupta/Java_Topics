1f461aa21c0afcdab35effb1a85ce2831190877a
Himanshu Kumar Gupta <himanshukumargupta135@gmail.com>
2025-07-07 03:04:19 +0530

    Enum as Inner Class

---
+++ Enums/Enum5Inner.java
@@ -0,0 +1,12 @@
+public class Enum5Inner {
+    enum Color {
+        RED,
+        GREEN,
+        BLUE;
+    }
+
+    public static void main(String[] args) {
+        Color c1 = Color.RED;
+        System.out.println(c1);
+    }
+}
