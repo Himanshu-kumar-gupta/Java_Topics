4c8db21c0afa767df13e7c97f3188040fa6f77b2
Himanshu Kumar Gupta <himanshukumargupta135@gmail.com>
2025-09-27 17:41:09 +0530

    Updates Enum3Color with main method

--- Enums/Enum3Color.java
+++ Enums/Enum3Color.java
@@ -7,5 +7,7 @@ public enum Enum3Color {
     public static void main(String[] args) {
         Enum3Color c1 = Enum3Color.RED;
         System.out.println(c1);
+        System.out.println(GREEN);
+        System.out.println(Enum3Color.BLUE);
     }
 }
